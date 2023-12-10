ca6be6ec799691bce3a563d6e80d5270caddde5c
faraygd <102520149+faraygd@users.noreply.github.com>
2023-12-11 00:51:53 +0700

    hotfix: remove beasiswa

--- app/dashboard/hooks/useDeleteBeasiswa.ts
+++ app/dashboard/hooks/useDeleteBeasiswa.ts
@@ -1,11 +1,10 @@
 import { axiosInstance } from "@/lib/axios";
-import { Beasiswa } from '@prisma/client'
 import { useMutation } from "@tanstack/react-query";
 
 export const useDeleteBeasiswa = () => {
   return useMutation({
     mutationFn: async (id: string) => {
-      const response = await axiosInstance.delete<Beasiswa>(
+      const response = await axiosInstance.delete(
         `/api/beasiswa/${id}`
       );
 
